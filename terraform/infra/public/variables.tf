ec37801153c344022fc9cf44ae701c0b2637f087
Helder Sepulveda <heldersepu@gmail.com>
2020-08-21 21:00:34 -0400

    Properly quote defaults

--- terraform/infra/public/variables.tf
+++ terraform/infra/public/variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "amis" {
   default = {
-    us-east-1 = "ami-60b6c60a"
-    us-west-2 = "ami-f0091d91"
+    "us-east-1" = "ami-60b6c60a"
+    "us-west-2" = "ami-f0091d91"
   }
 }
